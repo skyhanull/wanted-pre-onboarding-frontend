84e4b649f2ef3ca83d307a1211f363697bcb968e
skyhanull <anky0425@naver.com>
2023-02-04 20:51:35 +0900

    feat: 로그인페이지

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import Signin from "../components/Signin";
+import * as S from "../components/SignStyle";
+
+const Login = () => {
+  return (
+    <S.SignContent>
+      <Signin />
+      <div>aa</div>
+    </S.SignContent>
+  );
+};
+
+export default Login;
