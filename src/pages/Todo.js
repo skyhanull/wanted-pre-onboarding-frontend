d4b0105a5f4f5b24c3e03f160d66980ec793828a
skyhanull <anky0425@naver.com>
2023-02-05 01:06:58 +0900

    design: todopage css추가

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,10 +1,19 @@
 import TodoList from "../components/Todo/TodoList";
+import styled from "styled-components";
+
+const TodoPage = styled.main`
+  /* margin-top: 5rem; */
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+`;
 
 const Todo = () => {
   return (
-    <div>
+    <TodoPage>
       <TodoList />
-    </div>
+    </TodoPage>
   );
 };
 
