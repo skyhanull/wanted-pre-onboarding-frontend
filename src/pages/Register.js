96ab58e5e5a286e0fb825bbde62506737db0d9dc
skyhanull <anky0425@naver.com>
2023-02-04 20:55:06 +0900

    feat: signup페이지 생성

---
+++ src/pages/Register.js
@@ -0,0 +1,12 @@
+import Signup from "../components/Signup";
+import * as S from "../components/SignStyle";
+
+const Register = () => {
+  return (
+    <S.SignContent>
+      <Signup />
+    </S.SignContent>
+  );
+};
+
+export default Register;
