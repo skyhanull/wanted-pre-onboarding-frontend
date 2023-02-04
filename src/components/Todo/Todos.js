1d6b05f0ab96e2fc95dd5638c96b6b4a795d3b47
skyhanull <anky0425@naver.com>
2023-02-04 22:24:29 +0900

    feat: todos 컴포넌트 생성

---
+++ src/components/Todo/Todos.js
@@ -0,0 +1,128 @@
+import axios from "axios";
+import styled from "styled-components";
+import { useState } from "react";
+
+const TodosList = styled.li`
+  margin: 1rem;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const AAA = styled.label`
+  display: flex;
+  flex-wrap: nowrap;
+  margin: 1rem;
+`;
+
+const Todos = (data) => {
+  const { todo, isCompleted, id } = data.data;
+  const [modify, setModify] = useState(false);
+  const [modifyInput, setModifyInput] = useState("");
+  const [checked, setCkecked] = useState(isCompleted);
+  const access_token = localStorage.getItem("access_token");
+  const BASE_URL = "https://pre-onboarding-selection-task.shop";
+
+  const modifyHandler = (e) => {
+    setModifyInput(e.target.value);
+  };
+
+  const modifyBtn = () => {
+    setModify(!modify);
+  };
+
+  const checkedHandler = (event) => {
+    setCkecked(!isCompleted);
+    if (modify !== true) {
+      updateTodoList(event.target.checked, id, todo);
+    }
+  };
+
+  const updateTodoList = (checked, id, modifyInput) => {
+    axios({
+      method: "put",
+      url: `${BASE_URL}/todos/${id}`,
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-type": "application/json",
+      },
+      data: {
+        todo: modifyInput,
+        isCompleted: checked,
+      },
+    }).then(() => {
+      alert("수정되었습니다");
+    });
+  };
+
+  const updateHandler = (e) => {
+    e.preventDefault();
+    updateTodoList(checked, id, modifyInput);
+    setModify(false);
+  };
+
+  const deleteHandler = (e) => {
+    e.preventDefault();
+
+    axios({
+      method: "delete",
+      url: `${BASE_URL}/todos/${id}`,
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }).then(() => {
+      alert("삭제되었습니다");
+    });
+  };
+
+  return (
+    <TodosList>
+      <AAA>
+        <input type="checkbox" checked={checked} onChange={checkedHandler} />
+        {modify === true ? (
+          <div>
+            <input
+              type="text"
+              data-testid="modify-input"
+              onChange={modifyHandler}
+            ></input>
+            <button
+              type="submit"
+              onClick={updateHandler}
+              data-testid="submit-button"
+            >
+              제출
+            </button>
+            <button
+              type="button"
+              onClick={modifyBtn}
+              data-testid="cancel-button"
+            >
+              취소
+            </button>
+          </div>
+        ) : (
+          <div>
+            <span>{todo}</span>
+            <button
+              data-testid="modify-button"
+              type="button"
+              onClick={modifyBtn}
+            >
+              수정
+            </button>
+            <button
+              data-testid="delete-button"
+              type="submit"
+              onClick={deleteHandler}
+            >
+              삭제
+            </button>
+          </div>
+        )}
+      </AAA>
+    </TodosList>
+  );
+};
+
+export default Todos;
