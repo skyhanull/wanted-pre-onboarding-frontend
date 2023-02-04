a86e16194730df29bf7df320bff60ee954023cf4
skyhanull <anky0425@naver.com>
2023-02-05 01:06:27 +0900

    feat: main화면 버튼 생성

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const MainBtn = styled.button`
+  width: 30rem;
+  height: 10rem;
+  background-color: rgba(186, 232, 242, 0.6);
+  font-size: 40px;
+  margin: 20rem 1rem 1rem 7rem;
+  border: none;
+  border-radius: 20px;
+
+  &:hover {
+    background-color: rgba(36, 125, 194, 0.67);
+  }
+`;
 
 const Mainpage = () => {
   return (
     <div>
-      <button>
-        <Link to="/signup">메인</Link>
-      </button>
-      <button>
-        <Link to="/signin">메인</Link>
-      </button>
+      <Link to="/signup">
+        <MainBtn>signup</MainBtn>
+      </Link>
+
+      <Link to="/signin">
+        <MainBtn>signin</MainBtn>
+      </Link>
+
+      <Link to="/todo">
+        <MainBtn>todolist</MainBtn>
+      </Link>
     </div>
   );
 };
