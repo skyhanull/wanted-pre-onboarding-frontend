bd9d01853110874c735b513c3b63c15f4073190d
skyhanull <anky0425@naver.com>
2023-02-05 13:21:40 +0900

    refactor:mainpage수정

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,36 +1,5 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const MainBtn = styled.button`
-  width: 30rem;
-  height: 10rem;
-  background-color: rgba(186, 232, 242, 0.6);
-  font-size: 40px;
-  margin: 20rem 1rem 1rem 7rem;
-  border: none;
-  border-radius: 20px;
-
-  &:hover {
-    background-color: rgba(36, 125, 194, 0.67);
-  }
-`;
-
 const Mainpage = () => {
-  return (
-    <div>
-      <Link to="/signup">
-        <MainBtn>signup</MainBtn>
-      </Link>
-
-      <Link to="/signin">
-        <MainBtn>signin</MainBtn>
-      </Link>
-
-      <Link to="/todo">
-        <MainBtn>todolist</MainBtn>
-      </Link>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Mainpage;
