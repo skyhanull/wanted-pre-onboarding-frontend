fa2272f784c4935663cf4e080b6a1bd52ce5c4cc
skyhanull <anky0425@naver.com>
2023-02-05 13:17:37 +0900

    feat: todoapi 생성

---
+++ src/apis/TodoApi.js
@@ -0,0 +1,134 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}`,
+  headers: {
+    "Content-Type": "application/json ",
+  },
+});
+
+export const getTodos = async (setTodoLists) => {
+  const access_token = localStorage.getItem("access_token");
+  // axios({
+  //   method: "get",
+  //   url: `${process.env.REACT_APP_API_URL}/todos`,
+  //   headers: {
+  //     Authorization: `Bearer ${access_token}`,
+  //   },
+  // })
+  //   .then((res) => setTodoLists(res.data))
+  //   .catch((error) => {
+  //     throw new Error(error);
+  //   });
+  try {
+    const res = await API.get("todos", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    setTodoLists(res.data);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const postTodos = async (todo, setTodoLists, setTodoInput) => {
+  const access_token = localStorage.getItem("access_token");
+  // axios({
+  //   method: "post",
+  //   url: `${process.env.REACT_APP_API_URL}/todos`,
+  //   headers: {
+  //     Authorization: `Bearer ${access_token}`,
+  //     "Content-type": "application/json",
+  //   },
+  //   data: {
+  //     todo: todo,
+  //   },
+  // })
+  //   .then(() => {
+  //     setTodoInput("");
+  //     getTodos(setTodoLists);
+  //   })
+  //   .catch((error) => {
+  //     throw new Error(error);
+  //   });
+  try {
+    await API.post(
+      "todos",
+      {
+        todo: todo,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    setTodoInput("");
+    getTodos(setTodoLists);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const putTodos = async (id, modifyInput, checked, setTodoLists) => {
+  const access_token = localStorage.getItem("access_token");
+  // axios({
+  //   method: "put",
+  //   url: `${process.env.REACT_APP_API_URL}/todos/${id}`,
+  //   headers: {
+  //     Authorization: `Bearer ${access_token}`,
+  //     "Content-type": "application/json",
+  //   },
+  //   data: {
+  //     todo: modifyInput,
+  //     isCompleted: checked,
+  //   },
+  // }).then(() => {
+  //   alert("수정되었습니다");
+  //   getTodos(setTodoLists);
+  // });
+  try {
+    await API.put(
+      `todos/${id}`,
+      {
+        todo: modifyInput,
+        isCompleted: checked,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    alert("수정되었습니다");
+    getTodos(setTodoLists);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const deleteTodos = async (id, setTodoLists) => {
+  const access_token = localStorage.getItem("access_token");
+  // axios({
+  //   method: "delete",
+  //   url: `${process.env.REACT_APP_API_URL}/todos/${id}`,
+  //   headers: {
+  //     Authorization: `Bearer ${access_token}`,
+  //   },
+  // }).then(() => {
+  //   alert("삭제되었습니다");
+  //   getTodos(setTodoLists);
+  // });
+  try {
+    await API.delete(`todos/${id}`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    alert("삭제되었습니다");
+    getTodos(setTodoLists);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
